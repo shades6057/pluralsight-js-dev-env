17c0c22412c0e972c42e77877c45a9b9cf4da86a
shades6057 <shades6057@hotmail.com>
2017-08-23 23:17:47 -0500

    before bundle splitting tables still not working properly

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,27 +1,32 @@
-
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 
 export default {
-    debug: true,
-    devtool: 'inLine-source-map',
-    noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
+  debug: true,
+  devtool: 'inLine-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
     ],
     target: 'web',
     output: {
-        path: path.resolve(__dirname, 'src'),
-        publicPath: '/',
-        filename: 'bundle.js'
+      path: path.resolve(__dirname, 'src'),
+      publicPath: '/',
+      filename: 'bundle.js'
     },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'src')
-    },
-    plugins: [],
+    plugins: [
+      //Create HTML file that includes reference to bundles JS.
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        inject: true
+      })
+    ],
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
             {test: /\.css$/, loaders: ['style', 'css']}
         ]
-    }
-};
+  }
+}
+
