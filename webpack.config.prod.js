305b329b2831a5ae9d213444def7c9f28feeb7aa
shades6057 <shades6057@hotmail.com>
2017-08-25 15:51:04 -0500

    Productionbuild Module Complete

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,20 +2,35 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 export default {
     debug: true,
     devtool: 'source-map',
     noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
+    entry: {
+      vendor: path.resolve(__dirname, 'src/vendor'),
+      main: path.resolve(__dirname, 'src/index')
+    },
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+      // Generate an external css file with a hash in the filename
+      new ExtractTextPlugin('[name].[contenthash].css'),
+
+      // Hash the files using MD5 so that their names change when the content changes.
+      new WebpackMd5Hash(),
+
+      //Use CommonsChunkPlugin to create a separate bundle
+      //of vendor libraries so that they're cached separately.
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+
       //Create HTML file that includes reference to bundles JS.
       new HtmlWebpackPlugin({
         template: 'src/index.html',
@@ -31,18 +46,22 @@ export default {
           minifyCSS: true,
           minifyURLs: true
         },
-        inject: true
+        inject: true,
+          // Properties you define here are available in index.html
+          // using htmlWebpackPlugin.options.varName
+          trackJSToken: 'e119cc83346f4a439433f8bd42e639b5'
       }),
 
       //Eliminate duplicate packages when generating bundle
       new webpack.optimize.DedupePlugin(),
+
       //Minify JS
       new webpack.optimize.UglifyJsPlugin(),
     ],
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-            {test: /\.css$/, loaders: ['style', 'css']}
-        ]
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap')}
+          ]
     }
 };
